12a024ae3fa949f0ea1d7e3ecce56890f2607390
minori chikano <bcloudabc@gmail.com>
2023-06-03 18:13:07 +0900

    最初のコミット

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,28 @@
+class PicturesController < ApplicationController
+  def index
+    @pictures = Picture.all
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.create(picture_params)
+    if @picture.save
+      redirect_to pictures_path, notice: "作成しました"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:content)
+  end
+end
