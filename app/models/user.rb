988e1f37b8e538978d23a84fa143d18f31fc4e49
minori chikano <bcloudabc@gmail.com>
2023-06-04 16:21:32 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has many :pictures
-    validates :name, presence: true
-    validates :email, presence: true
+    validates :name, presence: true, length: { maximum: 30 }
+    validates :email, presence: true, length: {maximum: 255}
 end
