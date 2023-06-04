3bcd00625bc0af6dc9b1cbf2df8321a25ae9a5ec
minori chikano <bcloudabc@gmail.com>
2023-06-05 02:39:09 +0900

    ログインしていないユーザをログイン画面い飛ばす機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_form_forgecy with: :exception
   include SessionsHelper
+
+  private
+
+  def login_required
+    redirect_to new_session_path unless current_user
+  end
 end
