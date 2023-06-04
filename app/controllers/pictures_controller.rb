2cfa1b89a0f0a40ae23846344c0cb746862ed2f4
minori chikano <bcloudabc@gmail.com>
2023-06-04 12:18:31 +0900

    重複メソッドを共通化

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,6 @@
 class PicturesController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update]
+  
   def index
     @pictures = Picture.all
   end
@@ -17,11 +19,9 @@ class PicturesController < ApplicationController
   end
 
   def show
-    @picture = Picture.find(params[:id])
   end
 
   def edit
-    @picture = Picture.find(params[:id])
   end
 
   def update
@@ -32,10 +32,14 @@ class PicturesController < ApplicationController
       render :edit
     end
   end
-  
+
   private
 
   def picture_params
     params.require(:picture).permit(:content)
+  end
+
+  def set_blog
+    @blog = Picture.find(params[:id])
   end
 end
