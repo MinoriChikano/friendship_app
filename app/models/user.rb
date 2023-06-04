34f216358235330285cecd527e6f4edb6e3d4d3d
minori chikano <bcloudabc@gmail.com>
2023-06-05 00:51:09 +0900

    パスワードのハッシュ化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-    has many :pictures
-    validates :name,  presence: true, length: { maximum: 30 }
-    validates :email, presence: true, length: {maximum: 255}
-                      format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-                      before_validation { email.downcase! }
+ has many :pictures
+ validates :name,  presence: true, length: { maximum: 30 }
+ validates :email, presence: true, length: {maximum: 255}
+                   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+ before_validation { email.downcase! }
+ has_secure_password
 end
