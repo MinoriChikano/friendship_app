5d620dac1ee6ba8d1a4493d2013ddf52e51b8a02
minori chikano <bcloudabc@gmail.com>
2023-06-05 02:12:22 +0900

    createアクション実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      session[:use_id] = user.use_id
+      redirect_to user_path(user.id)
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
