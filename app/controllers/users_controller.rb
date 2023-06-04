2cf2e1a0d1fb79341758bd0c4e1b7fd0a6193da0
minori chikano <bcloudabc@gmail.com>
2023-06-05 01:33:26 +0900

    showアクション実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-        
+
     else
         render :new
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
