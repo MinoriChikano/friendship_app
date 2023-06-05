b5304b4ebd1f63bb0a0ba381dde9620ce34d1d69
minori chikano <bcloudabc@gmail.com>
2023-06-05 14:32:58 +0900

    画像機能実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -50,7 +50,7 @@ class PicturesController < ApplicationController
   private
 
   def picture_params
-    params.require(:picture).permit(:content)
+    params.require(:picture).permit(:content, :image, :image_cache)
   end
 
   def set_picture
