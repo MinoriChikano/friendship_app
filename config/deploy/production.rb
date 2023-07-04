506bd6988fc869e845fada96c76e083390d4b0ce
minori chikano <bcloudabc@gmail.com>
2023-07-05 03:30:58 +0900

    デプロイチェック

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '52.196.151.7', user: 'Minori Chikano', roles: %w{app db web}
+server '52.196.151.7', user: 'ec2-user', roles: %w{app db web}
 set :ssh_options, keys: '/Users/konnominoru/.ssh/id_rsa'
