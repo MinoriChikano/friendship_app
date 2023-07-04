78858f2e907d13961d1792bdb8ca020cbc44206f
minori chikano <bcloudabc@gmail.com>
2023-07-05 00:30:01 +0900

    確認

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'bcrypt'
 
 gem 'bootsnap', '>= 1.4.4', require: false
 
+gem 'rexml'
+
 group :development, :test do
 
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
@@ -36,6 +38,12 @@ group :development do
   gem 'listen', '~> 3.3'
 
   gem 'spring'
+
+  gem 'rspec-rails'
+  gem 'spring-commands-rspec'
+  gem 'factory_bot_rails'
+  gem 'faker'
+  gem 'launchy'
 end
 
 group :test do
@@ -48,3 +56,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'dotenv-rails'
+gem 'fog-aws'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_04_072832) do
+ActiveRecord::Schema.define(version: 2023_06_13_144820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "production_year"
+    t.integer "running_time"
+    t.text "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "pictures", force: :cascade do |t|
     t.text "content"
